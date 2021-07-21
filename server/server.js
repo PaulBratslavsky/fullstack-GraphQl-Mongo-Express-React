afb797aef5131ab9bb89e208bcc9b1a18bb4a144
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2021-07-21 03:36:52 -0500

    changed to use apollo server

--- server/server.js
+++ server/server.js
@@ -1,89 +1,74 @@
-const express = require('express')
-const app = express()
-const expressPlayground = require('graphql-playground-middleware-express')
-  .default
-const { graphqlHTTP } = require('express-graphql')
-const { buildSchema } = require('graphql')
-const mongose =  require('mongoose')
-const cors = require('cors')
+const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require('mongoose');
 
-const User = require('./models/user')
+const User = require('./models/user');
 
-app.use(cors())
-app.use(express.json())
-app.get('/graphql', expressPlayground({ endpoint: '/graphql' }))
-app.use('/graphql',graphqlHTTP({
-  schema: buildSchema(`
+const typeDefs = gql`
 
-    type RootQuery {
-      user(id: ID!): User!
-    }
+            type Query {
+                user(id:ID!): User!
+            }
 
-    type RootMutation {
-      addUser(userInput: UserInput!): User!
-    }
+            type Mutation{
+                addUser( userInput:UserInput!):User!
+            }
 
-    type User {
-      _id: ID!
-      password: String!
-      email: String!
-    }
+            type User {
+                _id: ID!
+                email: String!
+                password: String!
+            }
+            
+            input UserInput {
+                email: String!
+                password: String!
+            }    
+`;
 
-    input UserInput {
-      password: String!
-      email: String!
-    }
+const resolvers = {
 
-    schema {
-      query: RootQuery,
-      mutation: RootMutation,
-    }
-
-  `),
-  rootValue: {
-    
-    user: async args => {
-      try {
-
-        const user = await User.findOne({ _id:args.id })
-        return { ...user._doc }       
-
-      } catch (error) {
-        console.error("SERVER ERROR ", error)
-
-      }
+    Query:{
+        user:async (parent, args, context, info) =>{
+            try{
+                const user = await User.findOne({ _id:args.id });
+                return { ...user._doc }
+            } catch(err){
+                throw err
+            }
+        }
     },
 
-    addUser: async args => {
-      try {
+    Mutation:{
+        addUser:async (parent, args, context, info) => {
+            try {
+                const user = new User({
+                    email: args.userInput.email,
+                    password: args.userInput.password
+                });
+                const result = await user.save();
 
-        const user = new User({
-          email: args.userInput.email,
-          password: args.userInput.password,
-        })
-
-        const result = await user.save()
-        return { ...result._doc }
-
-      } catch (error) {
-        console.error("SERVER ERROR ", error)
-      }
+                return {
+                    ...result._doc
+                }
+            } catch(err){
+                throw err;
+            }
+        }
     }
+}
 
-  },
-  graphiql: true
-}))
+const server = new ApolloServer({ typeDefs, resolvers });
 
-const PORT = process.env.PORT || 5000
-mongose.connect(`mongodb+srv://CodingAdminThirty:kl453j6kl456j534m@cluster0.z8xy6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  app.listen(PORT, () => {
-    console.log("Running")
-  })
-}).catch(error => console.error("SERVER ERROR: ", error)
-) 
+const PORT = process.env.PORT || 5000;
 
-// mongodb+srv://CodingAdminThirty:<password>@cluster0.z8xy6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+mongoose.connect(`mongodb+srv://CodingAdminThirty:kl453j6kl456j534m@cluster0.z8xy6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>{
+    server.listen(PORT,()=>{
+        console.log(`Running running on port ${PORT}`)
+    });
+}).catch( err => {
+    console.log(err)
+});
 
